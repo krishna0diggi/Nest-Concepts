e998ad2d47ae7c57b00943f739ebffe52c8a0367
Krishna kr Saw <krishna.kumarsaw@diggibyte.com>
2026-02-01 11:44:10 +0530

    pipes-in-nest

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe, Post } from "@nestjs/common";
 
 @Controller("book")
 export class BookController{
     @Get()
     findAllBooks():string{
         return "This action returns all books"
-    }   
+    } 
+    
+    @Get("/:id")
+    findBookById(@Param("id", ParseIntPipe) id: number):string{
+        console.log(id, typeof(id))
+        return `This action returns a book by id: ${id}`
+    }
     @Post()
     createBook():string{
         return "This action creates a new book"
