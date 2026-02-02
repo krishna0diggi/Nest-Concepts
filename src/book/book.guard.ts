87d7f38cac1adf198ee6f07c94048729f12a97b7
Krishna kr Saw <krishna.kumarsaw@diggibyte.com>
2026-02-02 23:08:11 +0530

    guard-concept-with-payload-execution-context

--- src/book/book.guard.ts
+++ src/book/book.guard.ts
@@ -1,12 +1,19 @@
 import { CanActivate, Injectable } from "@nestjs/common";
 import { ExecutionContext } from "@nestjs/common";
 import { Observable } from "rxjs";
+import { Request } from "express";
 
 @Injectable()
 export class BookGuard implements CanActivate {
+    public key: string = "BCKLHFIEUJE"
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const ctx = context.switchToHttp();
+        const request  = ctx.getRequest<Request>();
+
+        if (request.header("key") !== this.key) return false
         console.log("Book guard called")
+        return request.header("key") === this.key
         // return false
-        return true;
+        // return true;
     }
 }
